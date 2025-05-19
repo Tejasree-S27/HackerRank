ffdf7833dd3091ca637d35f01cb573d19649d8bd
Tejasree-S27 <stejasree348@gmail.com>
2025-05-19 20:39:27 +0530

    Add files via upload

---
+++ JavaStaticInitializerBlock.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class JavaStaticInitializerBlock 
+{
+    static int B, H;
+    static boolean flag = true;
+
+    // Static block runs when the class is loaded
+    static
+    {
+        Scanner sc = new Scanner(System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+        sc.close();
+        
+        if (B <= 0 || H <= 0) 
+        {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+public static void main(String[] args)
+{
+		if(flag)
+        {
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
