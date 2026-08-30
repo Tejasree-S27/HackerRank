185df2d98c94c77b98f26a449586de9222dc76bc
Saligari Tejasree <stejasree348@gmail.com>
2026-08-30 15:28:16 +0530

    Create JavaRegexIPAddress.java

---
+++ JavaRegexIPAddress.java
@@ -0,0 +1,16 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+class Solution{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+    }
+}
+class MyRegex {
+    String pattern = 
+        "((25[0-5]|2[0-4][0-9]|1[0-9]{2}|0?[0-9]{1,2})\\.){3}" + "(25[0-5]|2[0-4][0-9]|1[0-9]{2}|0?[0-9]{1,2})";
+}
